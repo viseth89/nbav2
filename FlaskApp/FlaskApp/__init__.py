324c399819252dc27ee38252a1d3543043a1886c
viseth89 <viseth89@gmail.com>
2018-01-26 12:01:41 -0600

    Made it to 42minute mark, working in displaying data in index.html understanding functions made in 38minute mark

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -6,8 +6,69 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',title='Dialy Scores')
+    '''
+    Renders today's games on front page
+    '''
+    datetime_today = datetime.today()
+    pretty_date_today = datetime_today.strftime('%b %d, %Y')
+    games = get_games(datetime_today)
 
+    return render_template('index.html',title='Dialy Scores', games=games, pretty_date_today=pretty_date_today)
+
+def get_games(date):
+
+    '''
+    Get lists of games for the day
+
+    Args:
+        date : datetime object of the day that we want games.
+
+    Returns:
+        games: An array of dictionaries of the games that were played today
+    32:00 mark
+    '''
+
+
+    scoreboard = nba_py.Scoreboard(month=date.month,
+    day=date.day,
+    year=date.year)
+
+    line_score = scoreboard.line_score()
+
+
+
+    #list of games
+    games = []
+    #dictionary of the current game we're looking at
+    current_game = {}
+
+    current_game_sequence = 0
+
+    game_sequence_counter = 0
+
+    for team in line_score:
+        if (team['GAME_SEQUENCE'] != current_game_sequence):
+            current_game['TEAM_1_ABBREVIATION'] = team['TEAM_ABBREVIATION']
+            current_game['TEAM_1_WINS_LOSSES'] = team['TEAM_WINS_LOSSES']
+
+            current_game['TEAM_1_PTS'] = team['PTS']
+            current_game['TEAM_1_ID'] = team['TEAM_ID']
+
+            current_game_sequene = team['GAME_SEQUENCE']
+            game_sequence_counter += 1
+        elif (game_sequence_counter == 1):
+            current_game['TEAM_2_ABBREVIATION'] = team['TEAM_ABBREVIATION']
+            current_game['TEAM_2_WINS_LOSSES'] = team['TEAM_WINS_LOSSES']
+
+            current_game['TEAM_2_PTS'] = team['PTS']
+            current_game['TEAM_2_ID'] = team['TEAM_ID']
+
+            games.append(current_game)
+
+            current_game = {}
+            game_sequence_counter = 0
+
+        return games
 
 if __name__ == "__main__":
     # app.run(threaded=True)
