d0ad8a2c5306bea4ebb989ce15d0c20ea0f84309
viseth89 <viseth89@gmail.com>
2018-01-26 09:43:35 -0600

    Added icons for teams, added boostrap files, 18 minute mark

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+import nba_py
 
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    return "Hi there, how ya doin? We're gonna build the NBA site 2.0'
+def index():
+    return render_template('index.html',title='Dialy Scores')
 
 
 if __name__ == "__main__":
-    app.run()
+    # app.run(threaded=True)
+    app.run(host='0.0.0.0', port=8080, threaded=True, debug=True )
